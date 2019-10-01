d63696a9cf4d67554b66b09442ca35194e067836
DiamondYuan <admin@diamondyuan.com>
2019-10-01 09:52:15 +0800

    :wrench: config .vuepress/config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: 'Web Clipper',
+  description: 'Save anything on the web to anywhere',
+  themeConfig: {
+    nav: [{ text: 'Home', link: '/' }],
+  },
+  dest: 'public',
+};
